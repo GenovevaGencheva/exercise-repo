3fdee090d3150fea3886f50f479c2349bd16c17b
GenovevaGencheva <gencheva.genoveva@abv.bg>
2017-05-31 10:20:23 +0300

    add all

---
+++ AreaOfTriangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle
+{
+    class AreaOfTriangle
+    {
+        static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+            var area = a * h / 2;
+            Console.Write("Triangle area = ");
+            Console.WriteLine(Math.Round(area, 2));
+        }
+    }
+}
